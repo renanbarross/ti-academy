af8ba537f827d4de06646378a82d49817ef0894b
renanbarross <88800859+renanbarross@users.noreply.github.com>
2021-08-30 02:47:25 -0300

    Add files via upload

---
+++ ciclo2/aula5/atividades/script.js
@@ -0,0 +1,29 @@
+import {msgLog} from './modulos/modulos.js';
+
+
+window.onload = function(){
+//msgLog("ola");
+/*var data = new Date();
+
+var semana = ["dom", "seg", "ter", "qua", "qui", "sex", "sáb"];
+
+console.log(semana[data.getDay()]);
+
+var dta = data.toLocaleString('pt-BR', {year: 'numeric', month:'2-digit', day:'2-digit'});
+console.log(dta);*/
+
+/*var n =;
+
+try {
+
+	n = Number(n);
+	if(isNaN(n)) throw "Este valor não é um número";
+
+} catch(erro) {
+	console.log(`Erro: ${erro}`);
+} finally {
+	console.log("Terminando");
+}*/
+
+
+}
