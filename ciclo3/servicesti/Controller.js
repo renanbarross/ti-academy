33b25a8b19f8303d2f9f658d8514bcf907482805
renanbarross <88800859+renanbarross@users.noreply.github.com>
2021-09-03 10:20:09 -0300

    Consulta - Aula 4

--- ciclo3/servicesti/Controller.js
+++ ciclo3/servicesti/Controller.js
@@ -48,6 +48,104 @@ app.get('/pedidos', function(req,res){
     res.send('Lista de Pedidos.');
 });*/
 
+//EXERCÍCIO 1 - AULA 4
+/*app.get('/listaclientes', async(req,res)=>{
+    await cliente.findAll({
+        raw:true
+    }).then(function(clientes){
+        res.json({clientes})
+    });
+});*/
+
+//EXERCÍCIO 2 - AULA 4
+/*app.get('/listaclientes', async(req,res)=>{
+    await cliente.findAll({
+        order: [['CreatedAt']]
+    }).then(function(clientes){
+        res.json({clientes})
+    });
+});*/
+
+//EXERCÍCIO 3 - AULA 4
+/*app.get('/listapedidos', async(req,res)=>{
+    await pedido.findAll({
+        raw:true
+    }).then(function(pedidos){
+        res.json({pedidos})
+    });
+});*/
+
+//EXERCÍCIO 4 - AULA 4
+/*app.get('/listapedidos', async(req,res)=>{
+    await pedido.findAll({
+        order: [['valor', 'DESC']]
+    }).then(function(pedidos){
+        res.json({pedidos})
+    });
+});*/
+
+//EXERCÍCIO 5 - AULA 4
+/*app.get('/quantclientes', async(req,res)=>{
+    await cliente.count('id')
+    .then(function(clientes){
+        res.json(clientes);
+    });
+});*/
+
+//EXERCÍCIO 6 - AULA 4
+/*app.get('/quantpedidos', async(req,res)=>{
+    await pedido.count('id')
+    .then(function(pedidos){
+        res.json(pedidos);
+    });
+});*/
+
+app.get('/listaservicos', async(req,res)=>{
+    await servico.findAll({
+        order: [['nome', 'DESC']]
+    }).then(function(servicos){
+        res.json({servicos})
+    });
+});
+
+app.get('/ofertas', async(req,res)=>{
+    await servico.count('id')
+    .then(function(servicos){
+        res.json(servicos);
+    });
+});
+
+app.get('/servico/:id', async(req,res)=>{
+    servico.findByPk(req.params.id)
+    .then(servico =>{
+        return res.json({
+            error: false,
+            servico
+        });
+    }).catch(function(erro){
+        return res.status(400).json({
+            error: true,
+            message: "Código não está cadastrado!"
+        });
+    });
+});
+
+//desafio aula 4:
+app.get('/pedido/:id', async(req,res)=>{
+    pedido.sum('valor', { where: { 'ClienteId': req.params.id } })
+    .then(pedido =>{
+        return res.json({
+            error: false,
+            pedido
+        });
+    }).catch(function(erro){
+        return res.status(400).json({
+            error: true,
+            message: "Código não está cadastrado!"
+        });
+    });
+});
+
 let port=process.env.PORT || 3000;
 
 app.listen(port,(req,res)=>{
